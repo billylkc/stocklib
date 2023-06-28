4c92e74051527756e45288635406184b315dd84a
Billy Lam <billy.lamkc@gmail.com>
2023-06-28 08:12:02 +0800

    w/e

--- stock/hket.go
+++ stock/hket.go
@@ -3,6 +3,8 @@ package stock
 import "fmt"
 
 func HketTest() error {
+	// https://invest.hket.com/markets
+	// https://invest.hket.com/market-store/board_meeting/earnings_forecasts_bycode.html
 	fmt.Println("hket")
 
 	return nil
